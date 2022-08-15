c4a8338427c1881182c519766cf2d4185cf16c50
QAfun <arthur.popko@icloud.com>
2022-08-15 16:56:49 +0300

    Page object is added

--- tests/ui/test_google_search.py
+++ tests/ui/test_google_search.py
@@ -6,9 +6,9 @@ from tests.page_object.google_search_page import GoogleSearchPage
 
 @mark.dev
 def test_env_is_dev(cross_browser, app_config, env):
+    # open google search page:
     base_url = app_config.base_url
     port = app_config.app_port
-    # open google search page:
     google_search_page = GoogleSearchPage(driver=cross_browser)
     cross_browser.get(base_url)
     # input search request
