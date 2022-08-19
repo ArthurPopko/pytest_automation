d52cebd92675e2a3709a1ec8c200f19ae218f9b3
apopko <apopko@roadie.com>
2022-08-19 15:13:39 +0300

    ui qa test is fixed

--- tests/ui/test_google_search.py
+++ tests/ui/test_google_search.py
@@ -5,6 +5,7 @@ from tests.page_object.mail_search_page import MailSearchPage
 
 
 @mark.ui
+@mark.dev
 def test_google_search_env_dev(chrome_browser, app_config, env, load_test_data):
     # open google search page:
     base_url = app_config.base_url
@@ -25,12 +26,13 @@ def test_google_search_env_dev(chrome_browser, app_config, env, load_test_data):
 
 
 @mark.ui
+@mark.qa
 def test_mail_search_env_qa(chrome_browser, load_test_data, env, app_config):
     # open google search page:
     base_url = app_config.base_url
     port = app_config.app_port
     input_text = load_test_data.get('input1')
-    expected_text = 'data1'
+    expected_text = 'data11'
     mail_search_page = MailSearchPage(driver=chrome_browser)
     chrome_browser.get(base_url)
     # input search request
@@ -41,4 +43,4 @@ def test_mail_search_env_qa(chrome_browser, load_test_data, env, app_config):
     assert env == 'qa'
     assert base_url == 'http://mail.ru'
     assert port == 80
-    assert mail_search_page.search_input.attribute('value') == expected_text
+    assert mail_search_page.search_input_result.attribute('value') == expected_text
