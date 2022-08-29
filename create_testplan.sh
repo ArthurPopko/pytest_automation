b1ccdb0a9893642bb1e3aada4d8f32e435a7a7e1
QAfun <arthur.popko@icloud.com>
2022-08-29 15:43:09 +0300

    sh added

--- create_testplan.sh
+++ create_testplan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
-sed "$!N;s/Automated UI tests/Automated UI tests - $current_time/;P;D" testrail-ui.cfg >testrail-custom-ui.cfg
+sed "$!N;s/Automated UI tests/Automated UI tests $current_time/;P;D" testrail-ui.cfg >testrail-custom-ui.cfg
 #TEST_PLAN_INFO=$(
 #  curl -H "Content-Type: application/json" \
 #  -u "artur.popko@gmail.com:Stalker82!" \
